bc542b2fa572ed506ce71e5bea07a4bf90eef6b5
vanphapuit <87233323+vanphapuit@users.noreply.github.com>
2022-09-09 17:36:17 +0700

    up

--- data_structures/graph/g7.kahn_algorithm.cpp
+++ data_structures/graph/g7.kahn_algorithm.cpp
@@ -5,24 +5,46 @@ using namespace std;
 #define ll long long
 #define mod = 10e9+7
 
-template<class T>
-void swap(T *a, T *b){
-	T tmp;
-	tmp=*a;
-	*a=*b;
-	*b=tmp;
+int m,n;
+vector<int> adj[1001];
+vector<int> topo;
+int in[1001];
+
+void inp(){
+	cin>>n>>m;
+	for(int i=0;i<m;i++){
+		int x,y;
+		cin>>x>>y;
+		adj[x].push_back(y);
+		in[y]++;
+	}
+}
+
+void kahn(){
+	queue<int> q;
+	for(int i=1;i<=n;++i){
+		if(in[i] == 0)
+			q.push(i);
+	}
+	while(!q.empty()){
+		int u=q.front();
+		q.pop();
+		topo.push_back(u);
+		for(int v:adj[u]){
+			in[v]--;
+			if(in[v]==0)
+				q.push(v);
+		}
+	}
+	for(int x:topo)
+		cout<<x<<" ";
 }
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
-	int *a=NULL;
-
-	a=new int[6];
-	for(int i=0;i<6;i++) a[i]=i;
-	for(int i=0;i<7;i++) cout<<a[i]<<" ";
-	delete[] a;
+	inp();
+	kahn();
 	return 0;
 }
