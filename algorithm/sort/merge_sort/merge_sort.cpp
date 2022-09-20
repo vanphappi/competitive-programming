9b52b6a5a1fae1108c3526c63c29ac19b3fbe915
vanphapuit <87233323+vanphapuit@users.noreply.github.com>
2022-09-20 08:23:28 +0700

    up

--- algorithm/sort/merge_sort/merge_sort.cpp
+++ algorithm/sort/merge_sort/merge_sort.cpp
@@ -10,22 +10,51 @@ using namespace std;
 void merge(int a[], int l, int r, int m){
 	vector<int> x(a+l,a+m+1);
 	vector<int> y(a+m+1,a+r+1);
+	int i=0, j=0;
+	while(i < x.size() && j < y.size()){
+		if(x[i] >= y[j]){
+			a[l]=y[j];
+			j++;
+			l++;
+		}
+		else{
+			a[l]=x[i];
+			i++;
+			l++;
+		}
+	}
 
-	
+	while(i < x.size()){
+		a[l]=x[i];
+		i++;
+		l++;
+	}
+
+	while(j < y.size()){
+		a[l]=y[j];
+		j++;
+		l++;
+	}
+}
+
+void mergeSort(int a[], int l, int r){
+	if(l >= r)
+		return;
+	int m=(int)(l+r)/2;
+	mergeSort(a,l,m);
+	mergeSort(a,m+1,r);
+	merge(a,l,r,m);
 }
 
 int main()
 {
-	#ifndef ONLINE_JUDGE
- 
-    freopen("inputf.in", "r", stdin);
- 
-    freopen("outputf.out", "w", stdout);
-   
-	#endif
 
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
+	int a[6]={1,5,2,4,0,1};
+	mergeSort(a,0,5);
+	for(int x:a)
+		cout<<x<<" ";
 	return 0;
 }
--- slove.cpp
+++ slove.cpp
@@ -8,118 +8,17 @@ int dx[4] = {1,0,0,-1};
 int dy[4] = {0,-1,1,0};
 
 void slove(){
-	string s;
-	cin>>s;
-	stack<int> val;
-	stack<char> res;
-	int i=0;
-	while(i<=s.size()){
-		//cout<<i<<endl;
-		if(i<s.size() && isdigit(s[i])){
-			int num=0;
-			while(i<s.size() && isdigit(s[i])){
-				num=num*10+(s[i]-'0');
-				i++;
-			}
-			val.push(num);
-			i--;
-		}
-		else{
-			if(res.empty()){
-				//cout<<i<<endl;
-				res.push(s[i]);
-			}
-			else{
-				//cout<<i<<endl;
-				if(res.top()=='+'){
-					int k=val.top();
-					val.pop();
-					val.push(k+val.top());
-					res.pop();
-				}
-				else if(res.top()=='-'){
-					int k=val.top();
-					val.pop();
-					val.push(val.top()-k);
-					res.pop();
-				}
-				res.push(s[i]);
-			}
-		}
-		i++;
-	}
-	cout<<val.top()<<endl;
-	//res.pop_back();
-	//cout<<res.top()<<endl;
-}
 
-string decodeString(string s) {
-        string ans;
-        stack<char> st;
-        stack<char> check;
-        for(char i:s){
-            st.push(i);
-            if(i=='[')
-                check.push(i);
-            else if(i==']'){
-                check.pop();
-                st.pop();
-                if(!check.empty()){
-                    string tmp;
-                    while(st.top() != '['){
-                        tmp += to_string(st.top());
-                        st.pop();
-                    }
-                    st.pop();
-                    int n=stoi(to_string(st.top()));
-                    reverse(tmp.begin(),tmp.end());
-                    ans= tmp+ans;
-                    for(int k=0;k<n;++k)
-                        ans += ans;
-                }
-                else{
-                    string tmp;
-                    while(st.top() != '['){
-                        tmp += to_string(st.top());
-                        st.pop();
-                    }
-                    st.pop();
-                    int n=stoi(to_string(st.top()));
-                    reverse(tmp.begin(),tmp.end());
-                    for(int k=0;k<n;++k)
-                        tmp += tmp;
-                    
-                    if(!st.empty()){
-                        while(!st.empty()){
-                            tmp=to_string(st.top())+tmp;
-                            st.pop();
-                        }
-                    }
-                    ans += tmp;
-                }
-            }
-            
-        }
-        while(!st.empty()){
-            ans = to_string(st.top()) + ans;
-            st.pop();
-        }
-        return ans;
-    }
+}
 
 int main()
 {
-	#ifndef ONLINE_JUDGE
- 
-    freopen("inputf.in", "r", stdin);
- 
-    freopen("outputf.out", "w", stdout);
-   
-	#endif
+
 	
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	//slove();
-	cout<<decodeString("3[a]2[bc]");
+
+	int g=3;
+	cout<<++g*8;
 	return 0;
 }
